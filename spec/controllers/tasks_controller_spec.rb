c2c1b5d62d4fe5776a4e5355cfc9ba557b077bb4
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2018-01-11 04:15:45 +0000

    wrote test for creating json parsable task, followed the errors to the solution.

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -31,5 +31,16 @@ RSpec.describe TasksController, type: :controller do
             expect(task.done).to eq(true)
         end
     end
+    
+    describe "task#new" do
+        it "should allow a new task to be created" do 
+            post :create, task: {title: "Fix things"}
+            
+            expect(response).to have_http_status(:success)
+            response_value = ActiveSupport::JSON.decode(@response.body)
+            expect(response_value['title']).to eq("Fix things")
+            expect(Task.last.title).to eq("Fix things")
+        end
+    end
 
 end
